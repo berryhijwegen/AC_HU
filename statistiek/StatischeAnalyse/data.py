9d32d813fcf8218742c44eda3c80e87c64a1e9fa
Berry Hijwegen <berry.hijwegen@student.hu.nl>
2019-06-02 15:52:07 +0200

    Finished static analysis

---
+++ statistiek/StatischeAnalyse/data.py
@@ -0,0 +1,101 @@
+import csv, functions, matplotlib.pyplot as plt, numpy as np, re
+
+# Read data into variable data and note columns with non-floats in it.
+with open('resources/utrecht.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+
+    headers = {}
+    data = {}
+    nonNumericIndexes = []
+    for row in csv_reader:
+        if(line_count == 0):
+            for i in range(len(row)):
+                headers[row[i]] = i
+                data[i] = []
+        
+        else:
+            if 'JJ00' not in row[headers['Perioden']]:
+                i = 0
+                for element in row:
+                    try:
+                        float(element)
+                        data[i].append(float(element))
+                    except:
+                        data[i].append(element)
+                        if i not in nonNumericIndexes:
+                            nonNumericIndexes.append(i)
+                    i+=1
+        line_count+=1
+
+# List all columns with their index, let user choose 1.
+while(True):
+    for header in headers:
+        if headers[header] not in nonNumericIndexes and headers[header] is not 0:
+            print(f"{headers[header]}: {header}")
+
+    userInput = input("Choose a index: ")
+
+    index = int(userInput)
+
+    # Get key by value
+    if index in headers.values() and index not in nonNumericIndexes and index is not 0:
+        index = int(userInput)
+        for key, value in headers.items():
+            if value == index:
+                header = key
+
+        # Sort and show all data, functions in external file 'functions.py'
+        sortedData = functions.sort(data[index])
+
+        print(header)
+        print(f"Average:            \n{functions.average(sortedData)}")
+        _modus = functions.modus(sortedData)
+        print(f"Modus:              \n{_modus[0]}: {_modus[1]} time(s)")
+        print(f"Median:             \n{functions.median(sortedData)}")
+        print(f"Standard deviaton:  \n{functions.standardDeviation(sortedData)}")
+
+        # Show histogram with confindence interval
+        fig, ax = plt.subplots()
+
+        binwidth = (max(data[index]) - min(data[index])) / 40
+        
+        plt.hist(data[index], bins=np.arange(min(data[index]), max(data[index]) + binwidth, binwidth))
+        
+        avg = functions.average(data[index])
+        plt.axvline(x=avg, color='red')
+
+        for value in functions.confidenceInterval(data[index]):
+            plt.axvline(x=value, color='green', linestyle=':')
+
+        plt.ylabel('Count')
+        plt.xlabel('Value')
+        plt.title(re.sub(r"(\w)([A-Z])", r"\1 \2", header).split('_')[0])
+        plt.show()
+
+        # Show graph with all data and trend line
+        x, y, b = functions.linearRegression(data[index])
+        fig, ax = plt.subplots()
+        # plot points
+        plt.scatter(x, y, color = "m",marker = "o", s = 10) 
+
+        # predicted response vector 
+        y_pred = [b[0] + (e * b[1]) for e in x]
+    
+        # plot regression line
+        plt.plot(x, y_pred, color = "g")
+
+        plt.xlabel('Periode') 
+        plt.ylabel(re.sub(r"(\w)([A-Z])", r"\1 \2", header).split('_')[0]) 
+        plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+        ax.set_xticklabels(data[headers['Perioden']])
+        every_nth = 36
+        for n, label in enumerate(ax.xaxis.get_ticklabels()):
+            if n % every_nth != 0:
+                label.set_visible(False)
+        plt.title('Trend line')
+        plt.show()
+
+        break
+    else:
+        print('This index is not listed. Choose another.')
---
+++ statistiek/StatischeAnalyse/functions.py
@@ -0,0 +1,79 @@
+# Bubble sort algorithm, replace if bigger
+def sort(lst):
+    for iter_num in range(len(lst)-1,0,-1):
+        for i in range(iter_num):
+            if lst[i] > lst[i+1]:
+                temp = lst[i]
+                lst[i] = lst[i+1]
+                lst[i+1] = temp
+    return lst
+
+def length(lst):
+    l = 0
+    for item in lst:
+        l += 1
+    return l
+
+def total(lst):
+    _sum = 0
+    for item in lst:
+        _sum += item
+    return _sum
+
+def average(lst):
+    return total(lst) / length(lst)
+
+# Count and return highest
+def modus(lst):
+    hits = {}
+    highest = []
+    for value in lst:
+        if value in hits:
+            hits[value] += 1
+        else:
+            hits[value] = 1
+        
+        highest = [value, hits[value]] if  highest == [] or hits[value] > highest[1] else highest
+    return highest
+
+# return middle or average or 2 middle values, depends if even or odd
+def median(lst):
+    l = length(lst)
+    if l % 2 == 0:
+        total = (lst[int((l + 1) / 2 - 1.5)]) + (lst[int((l + 1) / 2 - 0.5)])
+        return total / 2
+    elif l % 2 == 1:
+        return lst[int((l+1)/2)]
+
+
+def standardDeviation(lst):
+    avg = average(lst)
+    quadrants = []
+    for element in lst:
+        quadrants.append((element - avg)**2)
+    return average(quadrants)**.5
+
+def confidenceInterval(lst):
+    # Calculate 95% confidence interval
+    std = standardDeviation(lst)
+    l = length(lst)
+    avg = average(lst)
+    return [avg + 1.96 * (std / (l**.5)), avg - 1.96 * (std / (l**.5))]
+
+
+def linearRegression(lst):
+    x = list(range(1, length(lst) + 1))
+    y = lst
+
+    n = length(x)
+    avg_x, avg_y = average(x), average(y) 
+  
+    # Calculating cross-deviation and deviation about x
+    SS_xy = total([a*b for a,b in zip(y,x)]) - n*avg_y*avg_x
+    SS_xx = total([a*b for a,b in zip(x,x)]) - n*avg_x*avg_x 
+
+    # Calculating regression coefficients 
+    b_1 = SS_xy / SS_xx
+    b_0 = avg_y - b_1*avg_x 
+  
+    return x, y, (b_0, b_1)
